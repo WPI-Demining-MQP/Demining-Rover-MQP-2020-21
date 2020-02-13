e11f3efccac8269c1476e816eb560e3f854ef6c5
Andrew VanOsten <avanosten@wpi.edu>
2020-02-12 19:24:48 -0500

    Added HW_Interface file (Teensy) and pin numbers

---
+++ HW_Interface/HW_Interface.ino
@@ -0,0 +1,22 @@
+#define L_LA_ENC_1    32  // Left linear actuator encoder input 1
+#define L_LA_ENC_2    31  // Left linear actuator encoder input 2
+#define R_LA_ENC_1    30  // Right linear actuator encoder input 1
+#define R_LA_ENC_2    29  // Right linear actuator encoder input 2
+#define L_LA_CTRL     23  // Left linear actuator control line
+#define R_LA_CTRL     22  // Right linear actuator control line
+#define CIM_CTRL      21  // CIM (slider) motor control line
+#define CIM_ENC_1     28  // CIM (slider) encoder line 1
+#define CIM_ENC_2     27  // CIM (slider) encoder line 2
+#define L_LIMIT       16  // Left limit switch line
+#define R_LIMIT       15  // Right limit switch line
+#define ULTRASONIC_1  1   // Ultrasonic 1 (trigger and echo)  ************************* on TX pin. Bad? ****************************************
+#define ULTRASONIC_2  2   // Ultrasonic 2 (trigger and echo)
+#define ULTRASONIC_3  3   // Ultrasonic 3 (trigger and echo)
+#define ULTRASONIC_4  4   // Ultrasonic 4 (trigger and echo)
+#define SERVO_1       20  // Servo 1 pin
+#define SERVO_2       14  // Servo 2 pin
+#define MD_INPUT      5   // Metal detector input
+
+void setup() {}
+
+void loop() {}
